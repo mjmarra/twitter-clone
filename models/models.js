0b97c2631155f584b6a4f592aa3f1a2fdcec9dc0
María José Marra <majomarra@gmail.com>
2020-10-19 17:50:55 -0300

    varios

--- models/models.js
+++ models/models.js
@@ -1,21 +1,21 @@
 /* CONNECTION DB */
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 mongoose.connect(
-  "mongodb+srv://root:root@ha-tweeter-clone.lcmro.mongodb.net/ha-tweeter-clone?retryWrites=true&w=majority"
+  'mongodb+srv://root:root@test.txpo3.mongodb.net/test?retryWrites=true&w=majority'
 );
 
 mongoose.connection
-  .once("open", () =>
-    console.log("¡Conexión con la base de datos establecida !!!!")
+  .once('open', () =>
+    console.log('¡Conexión con la base de datos establecida !!!!')
   )
-  .on("error", (error) => console.log(error));
+  .on('error', error => console.log(error));
 
 /* USER MODEL */
-const User = require("./User");
+const User = require('./User');
 
 /* TWEET MODEL */
-const Tweet = require("./Tweet");
+const Tweet = require('./Tweet');
 
 module.exports = {
   User,
